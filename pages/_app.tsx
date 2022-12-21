f5617ab9dfa933d8db34c528c00d1c2cac014e35
Minh Phat <82859683+MinhPhat01@users.noreply.github.com>
2022-12-21 18:07:47 +0700

    fix-error-deploy

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,41 +1,30 @@
-
 import "../styles/globals.css";
 import { SnackbarProvider } from "notistack";
 import NextNProgress from "nextjs-progressbar";
 import { AppProps } from "next/app";
 import Layout from "components/layout/Layout";
 import UI from "components/ErrorBoundary";
-import createEmotionCache from "./createEmotionCache";
-import { CacheProvider, EmotionCache } from "@emotion/react";
 import ComponentThemeProvider from "hocs/ThemeProvider";
 import SWR from "contexts/SWR";
 import Setting from "contexts/Settings";
 
 // Client-side cache, shared for the whole session of the user in the browser.
-const clientSideEmotionCache = createEmotionCache();
 
-interface MyAppProps extends AppProps {
-  emotionCache?: EmotionCache;
-}
-
-function MyApp(props: MyAppProps) {
-  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
+function MyApp({ Component, pageProps }: AppProps) {
   return (
     <UI>
       <SWR>
-        <CacheProvider value={emotionCache}>
-          <ComponentThemeProvider>
-            <Setting>
-              <SnackbarProvider>
-                <Layout>
-                  <NextNProgress color="#00A859" />
-                  <Component {...pageProps} />
-                </Layout>
-              </SnackbarProvider>
+        <ComponentThemeProvider>
+          <Setting>
+            <SnackbarProvider>
+              <Layout>
+                <NextNProgress color="#00A859" />
+                <Component {...pageProps} />
+              </Layout>
+            </SnackbarProvider>
+          </Setting>
+        </ComponentThemeProvider>
 
-            </Setting>
-          </ComponentThemeProvider>
-        </CacheProvider>
       </SWR >
     </UI>
   );
